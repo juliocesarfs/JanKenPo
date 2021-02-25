9849dbc77490bd4247605c1b1d471ab7e900c31a
Julio Cesar <juliocesarafs2@gmail.com>
2021-02-25 20:41:07 -0300

    feat: add server and main

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,34 @@
 package server;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server extends Thread {
+	
+	private String name;
+	private static ServerSocket server;
+	private Socket con;
+	private InputStream in;
+	private InputStreamReader inr;
+	private BufferedReader bfr;
+	private BufferedWriter bfw;
+	
+	
+	public Server(Socket con) {
+		this.con = con;
+		
+		try {
+			in = con.getInputStream();
+			inr = new InputStreamReader(in);
+			bfr = new BufferedReader(inr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
